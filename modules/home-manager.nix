ffcf05c860b57809a85a38672edbb027eea9f13b
KebabObama <lucaschyba@gmail.com>
2026-03-01 23:23:43 +0100

    maybe

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,15 +1,31 @@
 flake: {
+  config,
   lib,
   pkgs,
   ...
 }: let
+  cfg = config.programs.direnv.new;
   flakePkgs = flake.packages.${pkgs.stdenv.hostPlatform.system};
 in {
-  config = {
-    programs.direnv = {
-      enable = true;
-      package = lib.mkDefault flakePkgs.default;
-      nix-direnv.enable = lib.mkDefault true;
-    };
+  options.programs.direnv.new = {
+    enable = lib.mkEnableOption "direnv 'new' subcommand for scaffolding .envrc files";
   };
+
+  config = lib.mkMerge [
+    {
+      programs.direnv.settings = {
+        global = {
+          log_format = "";
+          hide_env_diff = true;
+        };
+      };
+    }
+    (lib.mkIf cfg.enable {
+      programs.direnv = {
+        enable = true;
+        package = lib.mkDefault flakePkgs.default;
+        nix-direnv.enable = lib.mkDefault true;
+      };
+    })
+  ];
 }
--- modules/nixos.nix
+++ modules/nixos.nix
@@ -4,13 +4,27 @@ flake: {
   pkgs,
   ...
 }: let
+  cfg = config.programs.direnv.new;
   flakePkgs = flake.packages.${pkgs.stdenv.hostPlatform.system};
 in {
-  config = {
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = lib.mkDefault true;
-    };
-    environment.systemPackages = [flakePkgs.default];
+  options.programs.direnv.new = {
+    enable = lib.mkEnableOption "direnv 'new' subcommand for scaffolding .envrc files";
   };
+
+  config = lib.mkMerge [
+    {
+      environment.etc."direnv/direnv.toml".text = ''
+        [global]
+        log_format = ""
+        hide_env_diff = true
+      '';
+    }
+    (lib.mkIf cfg.enable {
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = lib.mkDefault true;
+      };
+      environment.systemPackages = [flakePkgs.default];
+    })
+  ];
 }
