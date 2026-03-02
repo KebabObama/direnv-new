813c8749aa3dda6a841c197e6f28821814457c88
KebabObama <lucaschyba@gmail.com>
2026-03-02 07:56:41 +0100

    aaaa

--- flake.nix
+++ flake.nix
@@ -22,13 +22,26 @@
         enable = lib.mkEnableOption "direnv 'new' subcommand";
 
         templates = lib.mkOption {
-          type = lib.types.attrsOf lib.types.lines;
+          type = lib.types.attrsOf (
+            lib.types.oneOf [
+              lib.types.lines
+              (lib.types.submodule {
+                options.content = lib.mkOption {
+                  type = lib.types.lines;
+                  description = "Template content inserted into generated .envrc.";
+                };
+              })
+            ]
+          );
           default = {};
           example = {
-            "python" = ''
-              use flake
-              export PYTHONBREAKPOINT=ipdb.set_trace
-            '';
+            "flake" = {
+              content = ''
+                #!/usr/bin/env bash
+
+                use flake
+              '';
+            };
           };
           description = "Named templates available via direnv new -t/--template.";
         };
@@ -40,7 +53,12 @@
         declare -Ag DIRENV_NEW_TEMPLATES
       ''
       + lib.concatStringsSep "\n" (lib.mapAttrsToList (
-          name: content: "DIRENV_NEW_TEMPLATES[${lib.escapeShellArg name}]=${lib.escapeShellArg content}"
+          name: template: let
+            content =
+              if builtins.isAttrs template
+              then template.content
+              else template;
+          in "DIRENV_NEW_TEMPLATES[${lib.escapeShellArg name}]=${lib.escapeShellArg content}"
         )
         templates)
       + lib.optionalString (templates != {}) "\n";
